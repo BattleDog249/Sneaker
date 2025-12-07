63e7eda018893d16d89be5d644f6de19c81e55e4
Shlomo1412 <shlomofi050@gmail.com>
2025-12-07 21:21:02 +0200

    1.21.4 version first commit (initial files)

---
+++ 1.21.4/Sneaker/src/client/java/net/shlomo1412/sneaker/client/SneakerClient.java
@@ -0,0 +1,10 @@
+package net.shlomo1412.sneaker.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SneakerClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
